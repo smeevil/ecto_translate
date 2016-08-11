730cea91f53eb112ff2e0dfdf5fa3feb2f1fbb0b
Gerard de Brieder <smeevil@me.com>
2016-08-11 20:54:33 +0200

    updated semaphore config

--- config/semaphore.exs
+++ config/semaphore.exs
@@ -1,10 +1,15 @@
 use Mix.Config
 
-import_config "test.exs"
-
 config :ecto_translate, EctoTranslate.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   database: "ecto_translate_test",
   username: System.get_env("DATABASE_POSTGRESQL_USERNAME"),
   password: System.get_env("DATABASE_POSTGRESQL_PASSWORD")
+
+config :ecto_translate,
+  ecto_repos: [EctoTranslate.Repo],
+  repo: EctoTranslate.Repo,
+  gettext: EctoTranslate.Gettext
+
+config :logger, level: :info
