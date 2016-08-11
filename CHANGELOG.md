31465745633d3c06480dc0614d485bb13d82ea56
Gerard de Brieder <smeevil@me.com>
2016-08-11 21:20:11 +0200

    bumped deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
+## 0.2.1 (2016-07-02)
+  - bumped deps
+
 ## 0.2.0 (2016-07-02)
   - You can now pass a collection to translate, preventing n+1 queries.
-  
+
 ## 0.1.0 (2016-07-02)
 
   - Initial commit
--- mix.exs
+++ mix.exs
@@ -5,14 +5,16 @@ defmodule EctoTranslate.Mixfile do
     [
       app: :ecto_translate,
       description: "EctoTranslate is a library that helps with translating Ecto data. EctoTranslate can help you with returning translated values of your Ecto data attributes. For this it uses a singe table called 'translations' which will contain polymorphic entries for all of your Ecto data stucts.",
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
       aliases: aliases,
-      package: package
+      package: package,
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -27,12 +29,13 @@ defmodule EctoTranslate.Mixfile do
   defp applications(_), do: [:logger]
   defp deps do
     [
-      {:ecto, "~>2.0.0"},
-      {:gettext, ">0.0.0"},
-      {:postgrex, "> 0.0.0", only: [:dev, :test]},
-      {:credo, "> 0.0.0", only: [:dev, :test]},
-      {:earmark , "~> 0.2.1"  , only: :dev},
-      {:ex_doc  , "~> 0.12.0" , only: :dev}
+      {:ecto, "~>2.0.4"},
+      {:gettext, "~>0.11.0"},
+      {:postgrex, "~> 0.11.2", only: [:dev, :test]},
+      {:credo, "~> 0.4.8", only: [:dev, :test]},
+      {:earmark , "~> 1.0.1"  , only: :dev},
+      {:ex_doc  , "~> 0.13.0" , only: :dev},
+      {:excoveralls, "~> 0.5.5 ", only: :test},
     ]
   end
 
