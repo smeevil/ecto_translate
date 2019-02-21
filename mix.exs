3acb357e991354452895fad57fc21c06525bd966
Alexander Schramm <alex@schram.me>
2019-02-21 12:40:17 +0100

    fix: add ecto_sql to applications

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule EctoTranslate.Mixfile do
     [applications: applications(Mix.env())]
   end
 
-  defp applications(:test), do: [:logger, :postgrex]
+  defp applications(:test), do: [:logger, :ecto_sql, :postgrex]
   defp applications(_), do: [:logger]
 
   defp deps do
