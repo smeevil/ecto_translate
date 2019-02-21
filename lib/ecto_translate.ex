6df7f45fcc7da037c66997797b49113690f1dc67
Gerard de Brieder <smeevil@me.com>
2019-02-21 12:27:43 +0100

    Revert "chore: ecto 3 compatibility"

--- lib/ecto_translate.ex
+++ lib/ecto_translate.ex
@@ -67,112 +67,79 @@ defmodule EctoTranslate do
 
   For each of the functions the second parameter is an optional locale. if ommitted, it will use the current Gettext locale.
   """
-  @spec __using__(fields :: List.t()[Atom.t()]) :: nil
+  @spec __using__(fields :: List.t[Atom.t]) :: nil
   defmacro __using__(fields) do
     repo = Application.get_env(:ecto_translate, :repo)
 
-    translatable_fields_ast =
+    translatable_fields_ast = quote do
+      @docs"""
+      A simple helper funtion to return a list of translatable fields on this model
+      """
+      @spec translatable_fields :: List.t[Atom.t]
+      def translatable_fields, do: unquote(fields)
+    end
+
+    translated_field_ast = Enum.map(fields, fn field ->
       quote do
-        @docs """
-        A simple helper funtion to return a list of translatable fields on this model
+        @docs"""
+        Returns a translated value for the requested field in the optionally given locale.
+        If locale was ommitted, it will use the current Gettext locale.
+        This will cause a query to be run to get the translation.
         """
-        @spec translatable_fields :: List.t()[Atom.t()]
-        def translatable_fields, do: unquote(fields)
-      end
+        def unquote(:"translated_#{(field)}")(%{__meta__: %{source: {_,translatable_type}}, id: translatable_id} = model, locale \\ nil) do
+          locale = Atom.to_string(locale || String.to_atom(EctoTranslate.current_locale))
+          record = EctoTranslate
+          |> where(translatable_type: ^translatable_type, locale: ^locale, translatable_id: ^translatable_id, field: unquote(Atom.to_string(field)))
+          |> unquote(repo).one
 
-    translated_field_ast =
-      Enum.map(fields, fn field ->
-        quote do
-          @docs """
-          Returns a translated value for the requested field in the optionally given locale.
-          If locale was ommitted, it will use the current Gettext locale.
-          This will cause a query to be run to get the translation.
-          """
-          def unquote(:"translated_#{field}")(
-                %{__meta__: %{source: translatable_type}, id: translatable_id} = model,
-                locale \\ nil
-              ) do
-            locale = Atom.to_string(locale || String.to_atom(EctoTranslate.current_locale()))
-
-            record =
-              EctoTranslate
-              |> where(
-                translatable_type: ^translatable_type,
-                locale: ^locale,
-                translatable_id: ^translatable_id,
-                field: unquote(Atom.to_string(field))
-              )
-              |> unquote(repo).one
-
-            case record do
-              nil -> Map.get(model, unquote(field))
-              record -> Map.get(record, :content)
-            end
+          case record do
+            nil -> Map.get(model, unquote(field))
+            record -> Map.get(record, :content)
           end
         end
-      end)
-
-    translate_ast =
-      quote do
-        @docs """
-        Updates the model(s) that has/have been passed by replacing the content of the translatable fields with the optional locale given.
-        If locale was ommited, it will use the current Gettext locale.
-        This will cause a query to be run to get the translations.
-        """
-        @spec translate!(model :: Ecto.Schema.t() | List.t()[Ecto.Schema.t()], locale :: Atom.t()) ::
-                Ecto.Schema.t() | List.t()[Ecto.Schema.t()]
-        def translate!(model, locale \\ nil)
-
-        def translate!(
-              %{__meta__: %{source: translatable_type}, id: translatable_id} = model,
-              locale
-            ) do
-          locale = Atom.to_string(locale || String.to_atom(EctoTranslate.current_locale()))
-
-          translations =
-            EctoTranslate
-            |> where(
-              translatable_type: ^translatable_type,
-              translatable_id: ^translatable_id,
-              locale: ^locale
-            )
-            |> unquote(repo).all
-            |> Enum.map(fn record ->
-              {String.to_atom(Map.get(record, :field)), Map.get(record, :content)}
-            end)
-            |> Enum.into(%{})
-
-          Map.merge(model, translations)
-        end
-
-        def translate!([], _), do: []
-
-        def translate!(models, locale) when is_list(models) do
-          locale = Atom.to_string(locale || String.to_atom(EctoTranslate.current_locale()))
-          ids = Enum.map(models, fn model -> model.id end)
-          %{__meta__: %{source: translatable_type}} = Enum.at(models, 0)
-
-          translations =
-            EctoTranslate
-            |> where(translatable_type: ^translatable_type, locale: ^locale)
-            |> where([t], t.translatable_id in ^ids)
-            |> unquote(repo).all
-
-          # FIXME this might not be the most optimized way to do this, but for now, it works :)
-          Enum.map(models, fn model ->
-            attributes =
-              Enum.filter(translations, fn translation ->
-                translation.translatable_id == model.id
-              end)
-              |> Enum.map(fn record ->
-                {String.to_atom(Map.get(record, :field)), Map.get(record, :content)}
-              end)
-              |> Enum.into(%{})
-
-            Map.merge(model, attributes)
-          end)
-        end
       end
+    end)
+
+    translate_ast = quote do
+      @docs"""
+      Updates the model(s) that has/have been passed by replacing the content of the translatable fields with the optional locale given.
+      If locale was ommited, it will use the current Gettext locale.
+      This will cause a query to be run to get the translations.
+      """
+      @spec translate!(model :: Ecto.Schema.t | List.t[Ecto.Schema.t], locale :: Atom.t) :: Ecto.Schema.t | List.t[Ecto.Schema.t]
+      def translate!(model, locale \\ nil)
+      def translate!(%{__meta__: %{source: {_,translatable_type}}, id: translatable_id} = model, locale) do
+        locale = Atom.to_string((locale || String.to_atom(EctoTranslate.current_locale)))
+
+        translations = EctoTranslate
+        |> where(translatable_type: ^translatable_type, translatable_id: ^translatable_id, locale: ^locale)
+        |> unquote(repo).all
+        |> Enum.map(fn record -> {String.to_atom(Map.get(record, :field)) , Map.get(record, :content)} end)
+        |> Enum.into(%{})
+
+        Map.merge(model, translations)
+      end
+      def translate!([], _), do: []
+      def translate!(models, locale) when is_list(models) do
+        locale = Atom.to_string((locale || String.to_atom(EctoTranslate.current_locale)))
+        ids = Enum.map(models, fn model -> model.id end)
+        %{__meta__: %{source: {_,translatable_type}}} = Enum.at(models, 0)
+
+        translations = EctoTranslate
+        |> where(translatable_type: ^translatable_type, locale: ^locale)
+        |> where([t], t.translatable_id in ^ids)
+        |> unquote(repo).all
+
+        #FIXME this might not be the most optimized way to do this, but for now, it works :)
+        Enum.map(models, fn model ->
+          attributes = Enum.filter(translations, fn (translation) ->  translation.translatable_id == model.id end)
+          |> Enum.map(fn record -> {String.to_atom(Map.get(record, :field)) , Map.get(record, :content)} end)
+          |> Enum.into(%{})
+
+          Map.merge(model, attributes)
+        end)
+      end
+    end
 
     {translatable_fields_ast, {translated_field_ast, translate_ast}}
   end
@@ -197,11 +164,11 @@ defmodule EctoTranslate do
   def translatable_id_type, do: @translatable_id_type
 
   schema "translations" do
-    field(:translatable_id, @translatable_id_type)
-    field(:translatable_type, :string)
-    field(:locale, :string)
-    field(:field, :string)
-    field(:content, :string)
+    field :translatable_id, @translatable_id_type
+    field :translatable_type, :string
+    field :locale, :string
+    field :field, :string
+    field :content, :string
     timestamps()
   end
 
@@ -212,15 +179,13 @@ defmodule EctoTranslate do
   Builds a changeset based on the `struct` and `params` and validates the required fields and given locale
 
   """
-  @spec changeset(struct :: Ecto.Schema.t(), params :: Map.t()) :: Ecto.Changeset.t()
+  @spec changeset(struct :: Ecto.Schema.t, params :: Map.t) :: Ecto.Changeset.t
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields)
     |> validate_required(@required_fields)
     |> validate_locale
-    |> unique_constraint(:translatable_id,
-      name: :translations_translatable_id_translatable_type_locale_field_ind
-    )
+    |> unique_constraint(:translatable_id, name: :translations_translatable_id_translatable_type_locale_field_ind)
   end
 
   @doc """
@@ -237,11 +202,8 @@ defmodule EctoTranslate do
       ]
 
   """
-  @spec set(model :: Ecto.Schema.t(), options :: List.t()[{Atom.t(), Any.t()}]) ::
-          :ok | {:error, List.t()}
-  def set(%{__meta__: %{source: translatable_type}, id: translatable_id} = model, [
-        {:locale, locale} | options
-      ]) do
+  @spec set(model :: Ecto.Schema.t, options :: List.t[{Atom.t, Any.t}]) :: :ok | {:error, List.t}
+  def set(%{__meta__: %{source: {_,translatable_type}}, id: translatable_id} = model, [{:locale, locale} | options]) do
     params = %{
       translatable_type: translatable_type,
       translatable_id: translatable_id,
@@ -259,23 +221,19 @@ defmodule EctoTranslate do
   @doc """
   An helper method to get the current Gettext locale
   """
-  @spec current_locale :: String.t()
+  @spec current_locale :: String.t
   def current_locale, do: Gettext.get_locale(Application.get_env(:ecto_translate, :gettext))
 
   @doc """
   An helper method to get the known Gettext locales
   """
-  @spec known_locales :: List.t()[String.t()]
+  @spec known_locales :: List.t[String.t]
   def known_locales, do: Gettext.known_locales(Application.get_env(:ecto_translate, :gettext))
 
   defp validate_changesets(changesets) do
     case Enum.filter(changesets, fn changeset -> !changeset.valid? end) do
-      invalid when invalid == [] ->
-        {:ok, changesets}
-
-      _ ->
-        {:error,
-         Enum.map(changesets, fn changeset -> {changeset.changes.field, changeset.errors} end)}
+      invalid when invalid == [] -> {:ok, changesets}
+      _ -> {:error, Enum.map(changesets, fn changeset -> {changeset.changes.field, changeset.errors} end)}
     end
   end
 
@@ -283,32 +241,21 @@ defmodule EctoTranslate do
     options
     |> Enum.filter(fn {k, _v} -> Enum.member?(model.__struct__.translatable_fields, k) end)
     |> Enum.map(fn {field, content} ->
-      params = Map.merge(params, %{field: Atom.to_string(field), content: content})
-      EctoTranslate.changeset(%EctoTranslate{}, params)
+       params = Map.merge(params, %{field: Atom.to_string(field), content: content})
+       EctoTranslate.changeset(%EctoTranslate{}, params)
     end)
   end
 
   defp validate_locale(%{changes: %{locale: locale}} = changeset) do
-    case Enum.member?(EctoTranslate.known_locales(), locale) do
-      true ->
-        changeset
-
-      false ->
-        add_error(
-          changeset,
-          :locale,
-          "The locale '#{locale}' is not supported, supported are: #{
-            Enum.join(EctoTranslate.known_locales(), ", ")
-          }, if you think this is incorrect, make sure your Gettext.known_locales/1 knows about the locale you want to add..."
-        )
+    case Enum.member?(EctoTranslate.known_locales, locale) do
+      true  -> changeset
+      false -> add_error(changeset, :locale, "The locale '#{locale}' is not supported, supported are: #{Enum.join(EctoTranslate.known_locales, ", ")}, if you think this is incorrect, make sure your Gettext.known_locales/1 knows about the locale you want to add...")
     end
   end
-
   defp validate_locale(changeset), do: changeset
 
   defp upsert_translations([]), do: :ok
-
-  defp upsert_translations([changeset | changesets]) do
+  defp upsert_translations([changeset| changesets]) do
     case @repo.insert(changeset) do
       {:ok, cs} -> cs
       {:error, cs} -> cs |> update_translation
@@ -318,15 +265,9 @@ defmodule EctoTranslate do
   end
 
   defp update_translation(%{changes: changes}) do
-    record =
-      EctoTranslate
-      |> where(
-        translatable_type: ^changes.translatable_type,
-        translatable_id: ^changes.translatable_id,
-        locale: ^changes.locale,
-        field: ^changes.field
-      )
-      |> @repo.one!
+    record = EctoTranslate
+    |> where(translatable_type: ^changes.translatable_type, translatable_id: ^changes.translatable_id, locale: ^changes.locale, field: ^changes.field)
+    |> @repo.one!
 
     record = Ecto.Changeset.change(record, changes)
     @repo.update!(record)
